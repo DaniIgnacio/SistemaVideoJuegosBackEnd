4f50d51fb6ddd0eb9255179e5a8e280adf0da876
daniel fonseca <ignacio.fonseca2004@gmail.com>
2023-11-25 09:04:17 -0300

    carpeta static y icons

--- sistemaVideojuegos/settings.py
+++ sistemaVideojuegos/settings.py
@@ -129,5 +129,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
